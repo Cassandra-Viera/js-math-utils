ab1b9026b3b813e17e9bb5456c5a47f81f2efdf6
Cassandra Viera <fr.viera2@gmail.com>
2021-11-09 20:10:50 -0500

    const suma

---
+++ Challenge.js
@@ -0,0 +1,18 @@
+function Suma(a,b)  {
+    return (a + b);
+};
+
+Suma(7, 10);
+
+
+function suma(a, b) {
+    let resultado;
+    resultado = a + b;
+    return resultado;
+  }
+  
+  suma(3, 4);
+  suma(2, 15);
+
+console.log("Hola mundo");
+
