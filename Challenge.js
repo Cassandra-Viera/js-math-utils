506c7991cae6bfeff64880120644008e4c710946
Cassandra Viera <fr.viera2@gmail.com>
2021-11-11 15:02:58 -0500

    Funcion calcular promedio.

--- Challenge.js
+++ Challenge.js
@@ -12,3 +12,11 @@ function Restar(numero1, numero2)  {
     return resultado;
 };
 Restar(12, 5);
+
+
+function CalcularPromedio(numero1, numero2) {
+    const total = Sumar(numero1, numero2);
+    console.log(total/2);
+};
+
+CalcularPromedio(26, 2);
